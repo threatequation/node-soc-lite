b9d16dd85533599fbb983f695966e3d343a7f19b
Hirok__Orvi <hirok_orvi@yahoo.com>
2017-02-06 19:51:59 +0000

    index.js (socket.io middleware added)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var xss=require('./lib/xss');
+var socket= require('./lib/socket_xss.js');
 
 exports.ThreatEquationMiddleware = function(req,res){
      if (req.method === "GET"){
@@ -10,3 +11,11 @@ exports.ThreatEquationMiddleware = function(req,res){
        csrf(req);
      }
      };
+     socket.use(function(socket, next) {
+            var req = socket.handshake;
+            var res = {};
+            cookieParser(req, res, function(err) {
+                if (err) return next(err);
+                session(req, res, next);
+            });
+        });
