17fb4b40dcb8edd9eaa05a9c9ecf90d3c6cd8371
Hirok__Orvi <hirok_orvi@yahoo.com>
2017-02-18 17:59:41 +0000

    checking version added.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
 
+var checkVersion = require('./version')(process.version);
+if (!checkVersion) {
+    console.error('The current version of Node.js is ' + process.version + '.It\'s not support by us. Please upgrade it node>=4');
+}
+else {
 var xss=require('./lib/xss');
-var socket= require('./lib/socket_xss.js');
+var socket= require('./lib/socket_xss');
 
 exports.ThreatEquationMiddleware = function(req,res){
      if (req.method === "GET"){
@@ -19,3 +24,4 @@ exports.ThreatEquationMiddleware = function(req,res){
                 session(req, res, next);
             });
         });
+}
