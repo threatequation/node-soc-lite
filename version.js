ebeb7628f53c05d2507c9543c1b8084bb4270d81
hirokbiswas <hirokgreen@interconnectionbd.com>
2018-08-06 12:07:47 +0600

    .gitignore is now working

--- version.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// it will check the version of user
-module.exports = function (version) {
-    var version_number = version.match(/(\d+)/)[1];
-    return parseInt(version_number) >= 4;
-};
