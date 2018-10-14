5ace2bb805a6a690bb8170eb2613ce5b1b9a8364
greenhirok <biswashirok@gmail.com>
2018-10-14 17:59:39 +0600

    added log generator and sql module

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var xss = require('./lib/xss');
+var sql = require('./lib/sql');
 // var socket= require('./lib/socket_xss');
 
 exports.ThreatEquationMiddleware = function (req, res) {
 	xss(req, res);
+	sql(req, res);
 };
